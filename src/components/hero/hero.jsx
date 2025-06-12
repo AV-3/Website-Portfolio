8241822b34ccaec2f85697fc1a8d6c6e9c98519d
Alan <avele86@wgu.edu>
2025-06-12 00:35:50 -0500

    dev complete: navbar; in progress: hero section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import "./hero.css";
 
 const Hero = () => {
   return (
     <section className="hero" id="hero">
-      <div className="hero__container">
-        <h1 className="hero__title">Welcome to My Portfolio</h1>
+      <div className="container hero__container">
+        <h3>Hi, my name is</h3>
+        <h1 className="hero__title">Alan Velez .</h1>
         <p className="hero__description">
-          I am a passionate software developer with a love for creating innovative solutions. Explore my work and let's connect!
+          I am a passionate software engineer with a love for creating innovative solutions. Explore my work and let's connect!
         </p>
         <a href="#contact" className="hero__button">Get in Touch</a>
       </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import {useState} from 'react';
 import './navbar.css';
 import LogoImage from '../../assets/av-high-resolution-logo-color-on-transparent-background.png';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    const closeMobileMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <nav className="navbar">
         <div className="navbar__container">
-            <a href="#hero" className="navbar__logo"><img src={LogoImage} alt="Alan Velez" /></a>
-            <ul className="navbar__menu">
-            <li className="navbar__item"><a href="#hero" className="navbar__link">Home</a></li>
-            <li className="navbar__item"><a href="#about" className="navbar__link">About</a></li>
-            <li className="navbar__item"><a href="#skills" className="navbar__link">Skills</a></li>
-            <li className="navbar__item"><a href="#projects" className="navbar__link">Projects</a></li>
-            <li className="navbar__item"><a href="#experience" className="navbar__link">Experience</a></li>
-            <li className="navbar__item"><a href="#contact" className="navbar__link">Contact</a></li>
+            <a href="#hero" className="navbar__logo" onClick={closeMobileMenu}><img src={LogoImage} alt="Alan Velez" /></a>
+
+            <button className={`hamburger__menu ${isMenuOpen ? 'is-open' : ''}`} onClick={toggleMenu}>
+              <div className="hamburger__bar"></div>
+              <div className="hamburger__bar"></div>
+              <div className="hamburger__bar"></div>
+            </button>
+
+            <ul className={`navbar__menu ${isMenuOpen ? 'is-open' : ''}`}>
+            <li className="navbar__item"><a href="#hero" className="navbar__link" onClick={closeMobileMenu}>Home</a></li>
+            <li className="navbar__item"><a href="#about" className="navbar__link" onClick={closeMobileMenu}>About</a></li>
+            <li className="navbar__item"><a href="#skills" className="navbar__link" onClick={closeMobileMenu}>Skills</a></li>
+            <li className="navbar__item"><a href="#projects" className="navbar__link" onClick={closeMobileMenu}>Projects</a></li>
+            <li className="navbar__item"><a href="#experience" className="navbar__link" onClick={closeMobileMenu}>Experience</a></li>
+            <li className="navbar__item"><a href="#contact" className="navbar__link" onClick={closeMobileMenu}>Contact</a></li>
             </ul>
         </div>
         </nav>
