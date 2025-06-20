13ec6bcfdaba902541a6777203a8b7fc85d110da
Alan <avele86@wgu.edu>
2025-06-19 23:19:12 -0500

    created placeholder for future projects in projects section. Mobile dev complete.

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -16,15 +16,16 @@ const About = () => {
           Before my career in software, I served over 7 years in the <span className="highlight-gold">U.S. Air Force</span>, 
           where I honed my skills in leadership, teamwork, and problem-solving under pressure.
         </p>
-        <h3 className="about__subtitle">Education</h3>
+        <h2 className="about__subtitle">Education</h2>
         <div className="about__education__container first__education__container">
           <h3>Georgia Institute of Technology</h3>
-          <p>Master of Science in Computer Science</p>
+          <h4>Master of Science in Computer Science</h4>
+          <h5>Computer Perception & Robotics Specialization</h5>
           <p>Incoming: 2025</p>
         </div>
         <div className="about__education__container">
-          <h2>Western Governors University</h2>
-          <p>Bachelor of Science in Computer Science</p>
+          <h3>Western Governors University</h3>
+          <h4>Bachelor of Science in Computer Science</h4>
           <p>Graduated: 2025</p>
         </div>
       </div>
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,13 +6,10 @@ const Projects = () => {
         <section className="projects" id="projects">
         <div className="projects__container">
             <h2 className="projects__title">My Projects</h2>
-            <p className="projects__description">
-            Here are some of the projects I have worked on, showcasing my skills in software development and design.
-            </p>
             <ul className="projects__list">
-            <li className="projects__item">Project 1: A web application built with React and Node.js.</li>
-            <li className="projects__item">Project 2: A mobile app developed using React Native.</li>
-            <li className="projects__item">Project 3: An e-commerce platform with a custom backend.</li>
+                <li className="projects__list__item">Project 1: Placeholder for embedded systems project.</li>
+                <li className="projects__list__item">Project 2: Placeholder for robotics project</li>
+                <li className="projects__list__item">Project 3: Placeholder for AI/ML project</li>
             </ul>
         </div>
         </section>
